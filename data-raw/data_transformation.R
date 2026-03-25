681d2d7c549598e19d62d833d0f23562f418f846
andreh0v <fredrikandrebertelsen@gmail.com>
2026-03-25 14:29:41 +0100

    update files

--- data-raw/data_transformation.R
+++ data-raw/data_transformation.R
@@ -1,3 +1,7 @@
+# Inititate or create a data-raw folder inside the package project
+# usethis::use_data_raw()
+# One way (among many ways) to edit the file is by in the console of RStudio execute
+file.edit("data-raw/data_transformation.R")
 #devtools::install_github("dickoa/gretlReadWrite")
 usedvw <- gretlReadWrite::read.gdt("gdt/usedvw.gdt")
 newcars <- gretlReadWrite::read.gdt("gdt/newcars.gdt")
